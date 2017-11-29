83a92c88d07899cb0292b19223e9edbac3ee10c1
nicolaGuerrieri <guerrierinicola@gmail.com>
2017-11-29 14:37:26 +0100

    impianto

---
+++ src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { UtilityService } from '../../providers/utility-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [UtilityService]
+})
+export class HomePage {
+
+  coords: any;
+  accuracy: any;
+  error: any;
+  userData: any = null;
+  constructor(public navCtrl: NavController, public utilityService: UtilityService, private facebook: Facebook) {
+    this.searchLocation();
+  }
+
+  searchLocation() {
+    this.utilityService.searchLocation().then(data => {
+      this.coords = data;
+    });
+  }
+
+  loginWithFb() {
+    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+        this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] }
+      })
+    })
+  }
+
+
+}
---
+++ src/providers/utility-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Geolocation } from 'ionic-native';
+
+
+/*
+  Generated class for the UtilityService provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UtilityService {
+  coords: any;
+  accuracy: any;
+  error: any;
+  constructor() {
+    console.log('Hello UtilityService Provider');
+  }
+
+  searchLocation() {
+
+    return new Promise(resolve => {
+ 
+      Geolocation.getCurrentPosition().then((position) => {
+        this.coords = position.coords.latitude + " " + position.coords.longitude;
+        resolve(this.coords);
+
+      }, (err) => {
+        alert("Please enable GPS localization");
+        resolve(err);
+      });
+    });
+
+  }
+
+}
